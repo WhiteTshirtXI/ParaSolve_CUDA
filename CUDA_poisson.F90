24c65e2bb8e9cd31f10f7bcb2bf38763f6caf80b
akash <akashdhruv@gwmail.gwu.edu>
2016-08-19 13:04:41 -0500

    CUDA_poisson changes

--- CUDA_poisson.F90
+++ CUDA_poisson.F90
@@ -7,19 +7,20 @@ attributes(global) subroutine CUDA_poisson(ps,ps_old,ps_RHS,gr_dx_centers,gr_dy_
 
     integer*4 :: i,j,n(2)
 
-    i = (blockIdx%x-1)*blockDim%x + threadIdx%x
-    j = (blockIdx%y-1)*blockDim%y + threadIdx%y
+    i = (blockIdx%x-1)*blockDim%x + threadIdx%x + 1
+    j = (blockIdx%y-1)*blockDim%y + threadIdx%y + 1
 
     n(1) = size(ps,1)
     n(2) = size(ps,2)
 
-    if (i<=n(1) .and. j<=n(2)) then
+    if (i<n(1) .and. j<n(2)) then
 
            ps(i,j)=((ps_old(i,j+1)/(gr_dy_centers(i,j)*gr_dy_nodes(i,j)))+(ps(i,j-1)/(gr_dy_nodes(i,j)*gr_dy_centers(i-1,j-1)))&
                   +(ps_old(i+1,j)/(gr_dx_centers(i,j)*gr_dx_nodes(i,j)))+(ps(i-1,j)/(gr_dx_nodes(i,j)*gr_dx_centers(i-1,j-1)))&
                   +ps_RHS(i-1,j-1))&
                   *(1/((1/(gr_dx_nodes(i,j)*gr_dx_centers(i-1,j-1)))+(1/(gr_dy_nodes(i,j)*gr_dy_centers(i-1,j-1)))+&
                    (1/(gr_dx_nodes(i,j)*gr_dx_centers(i,j)))+(1/(gr_dy_nodes(i,j)*gr_dy_centers(i,j)))))
+
 
     end if
 
